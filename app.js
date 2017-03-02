0de4adbcc6d973d53df4d20e31579e60105972b7
Kyler Chin <kylerschin@users.noreply.github.com>
2017-03-01 18:05:05 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// curl -k https://localhost:8000/
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8000);
